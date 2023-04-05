b3aca0abf12f6425dfb9b29bbfa4600fece4be8e
Erin Keller <erinkeller@Erins-MacBook-Pro-2.local>
2023-04-05 14:51:31 -0400

    Served up the css/js files from express server and modified some of the css in styles.css

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// serve client side files to web pages (css/images/etc)
+app.use(express.static(path.join(__dirname, '/public')));
+
 // sendFile will go here
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '/index.html'));
+  res.sendFile(path.join(__dirname, '/views/index.html'));
 });
 
 app.listen(port);
